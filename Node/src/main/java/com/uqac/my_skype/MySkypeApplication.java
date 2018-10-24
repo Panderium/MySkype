f23d00babe527dcb9d5acbbc70024a7d899277ff
Alexis Fougerouse <fougerouse.a@gmail.com>
2018-10-24 12:53:07 -0400

    first commit

---
+++ Node/src/main/java/com/uqac/my_skype/MySkypeApplication.java
@@ -0,0 +1,12 @@
+package com.uqac.my_skype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySkypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySkypeApplication.class, args);
+	}
+}
---
+++ Node/src/main/java/com/uqac/my_skype/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.uqac.my_skype;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MySkypeApplication.class);
+	}
+
+}
